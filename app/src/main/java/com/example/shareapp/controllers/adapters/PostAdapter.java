2f172e67204602fc562bdb0208cf69f7af38d137
emil <hoangominh01@gmail.com>
2023-10-11 01:02:42 +0700

    example adapter

---
+++ app/src/main/java/com/example/shareapp/controllers/adapters/PostAdapter.java
@@ -0,0 +1,35 @@
+package com.example.shareapp.controllers.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.shareapp.models.Post;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends ArrayAdapter {
+    private Context context;
+    private int resource;
+    private ArrayList<Post> posts;
+
+    public PostAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Post> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.posts = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        convertView = LayoutInflater.from(context).inflate(resource, parent, false);
+        // @todo: set item view logic here
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/shareapp/models/Post.java
@@ -0,0 +1,6 @@
+package com.example.shareapp.models;
+
+public class Post {
+
+    // @todo: add post properties here
+}
